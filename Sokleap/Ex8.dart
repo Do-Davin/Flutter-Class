75be0314a7c34c2dbee4a55da607c194f8d76056
Sokleap-SAM <leapngu2019@gmail.com>
2025-09-12 11:29:33 +0700

    Submit Ex8

---
+++ Sokleap/Ex8.dart
@@ -0,0 +1,148 @@
+import 'dart:io';
+import 'dart:math';
+
+class PasswordGenerator {
+  static const String upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  static const String lower = "abcdefghijklmnopqrstuvwxyz";
+  static const String numbers = "0123456789";
+  static const String symbols = "!@#\$%^&*()";
+  final Random _random = Random();
+
+  String generatePassword(
+    int passLength,
+    bool isUsingUpper,
+    bool isUsingLower,
+    bool isUsingNumber,
+    bool isUsingSymbol,
+  ) {
+    List<String> password = [];
+    String randomPool = "";
+    if (isUsingUpper) {
+      password.add(upper[_random.nextInt(upper.length)]);
+      randomPool += upper;
+    }
+    if (isUsingLower) {
+      password.add(lower[_random.nextInt(lower.length)]);
+      randomPool += lower;
+    }
+    if (isUsingNumber) {
+      password.add(numbers[_random.nextInt(numbers.length)]);
+      randomPool += numbers;
+    }
+    if (isUsingSymbol) {
+      password.add(symbols[_random.nextInt(symbols.length)]);
+      randomPool += symbols;
+    }
+    while (password.length < passLength) {
+      password.add(randomPool[_random.nextInt(randomPool.length)]);
+    }
+    password.shuffle();
+    return password.join();
+  }
+}
+
+void clearScreen() {
+  try {
+    // Try ANSI escape code first ()
+    stdout.write("\x1B[2J\x1B[0;0H");
+  } catch (e) {
+    // If ANSI failed, try using shell
+    if (Platform.isWindows) {
+      Process.runSync("cls", [], runInShell: true);
+    } else {
+      Process.runSync("clear", [], runInShell: true);
+    }
+  }
+  // Note: some old version might work with shell but not ANSI
+}
+
+void pause() {
+  print("\nPress any key to continue...");
+  stdin.readByteSync(); // accept any key
+}
+
+void menu() {
+  print("=== Password Generator ===");
+  print("1. Generate Password");
+  print("2. Exit");
+  print("-------------------------------");
+}
+
+int validateNumberInput(String prompt) {
+  while (true) {
+    stdout.write(prompt);
+    String? input = stdin.readLineSync();
+    int? choice = int.tryParse(input ?? "");
+    if (choice != null) {
+      return choice;
+    }
+    print("⚠️  Invalid number! Please try again\n");
+  }
+}
+
+bool validateYesNoAnswerInput(String prompt) {
+  while (true) {
+    stdout.write(prompt);
+    String? input = stdin.readLineSync()?.toLowerCase();
+    if (input == "y") {
+      return true;
+    }
+    if (input == "n") {
+      return false;
+    }
+    print("⚠️  Invalid input! Please enter 'y' or 'n'\n");
+  }
+}
+
+void main() {
+  int choice = -1;
+  PasswordGenerator passwordGenerator = new PasswordGenerator();
+  while (choice != 2) {
+    clearScreen();
+    menu();
+    choice = validateNumberInput("Enter your choice: ");
+    switch (choice) {
+      case 1:
+        int passLength = validateNumberInput(
+          "👉 Enter password length (min 6): ",
+        );
+        if (passLength < 6) {
+          print("⚠️  Password length must be at least 6!");
+          break;
+        }
+
+        bool isUsingUpper = validateYesNoAnswerInput(
+          "Include uppercase letters? (y/n): ",
+        );
+        bool isUsingLower = validateYesNoAnswerInput(
+          "Include lowercase letters? (y/n): ",
+        );
+        bool isUsingNumber = validateYesNoAnswerInput(
+          "Include numbers? (y/n): ",
+        );
+        bool isUsingSymbol = validateYesNoAnswerInput(
+          "Include symbols? (y/n): ",
+        );
+        if (!(isUsingUpper || isUsingLower || isUsingNumber || isUsingSymbol)) {
+          print("⚠️  You must select at least one character type!");
+          break;
+        }
+
+        String pass = passwordGenerator.generatePassword(
+          passLength,
+          isUsingUpper,
+          isUsingLower,
+          isUsingNumber,
+          isUsingSymbol,
+        );
+        print("✅ Generated Password: $pass");
+        pause();
+        break;
+      case 2:
+        print("👋 Exiting Password Generator. Goodbye!");
+        break;
+      default:
+        print("⚠️  Incorrect number! Please try again\n");
+    }
+  }
+}
