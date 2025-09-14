8f96e4d15f05e82755788e6648558db4c9c3a520
KheangAnn <Kheangann@gmail.com>
2025-09-14 14:11:17 +0700

    update Ex6 and done Ex7

--- Ann/Ex6.dart
+++ Ann/Ex6.dart
@@ -1,13 +1,12 @@
 import 'dart:io';
 
 Future<void> printorderMessage() async {
-  print("Countdown is starting now :");
   var count = await fetchUserOrder();
-  print("Let's count right now: $count");
+  print("Countdown is complete right now, $count");
 }
 
-Future<String> fetchUserOrder() {
-  return Future.delayed(const Duration(seconds: 2), () => "Happy New Year!");
+Future<String> fetchUserOrder() async{
+  return "Happy New Year!";
 }
 
 Future<void> countdown(int n) async {
